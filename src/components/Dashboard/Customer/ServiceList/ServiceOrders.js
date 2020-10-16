1d254c93a825f50439b964939702512a4a7566ae
mdahmedmaruf <mdahmedmaruf@gmail.com>
2020-10-17 00:52:24 +0600

    small changes

--- src/components/Dashboard/Customer/ServiceList/ServiceOrders.js
+++ src/components/Dashboard/Customer/ServiceList/ServiceOrders.js
@@ -1,19 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../../../App';
 import ServiceOrderDetails from './ServiceOrderDetails';
 
 const ServiceOrders = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     const [serviceOrders, setServiceOrders] = useState([]);
     
+    
+
     useEffect(() => {
-        fetch('https://sheltered-savannah-44283.herokuapp.com/AllOrderLists')
+        fetch('https://sheltered-savannah-44283.herokuapp.com/userOrder?email='+loggedInUser.email, {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'}
+        })
         .then(res => res.json())
         .then(data => setServiceOrders(data));
-    }, []);
+    }, [])
 
     return (
         <div className="service-orders">
             <div className="container">
                 <div className="row mx-3">
+                {
+                        serviceOrders.length > 0 ?
+                        <p className="text-center">You have Placed {serviceOrders.length} Order! </p>
+                        :
+                        <p className="text-center">You haven't Placed any Order Yet!</p>
+                    }
                     {
                         serviceOrders.map(serviceOrder => <ServiceOrderDetails serviceOrder={serviceOrder} key={serviceOrder.name} />)
                     }
